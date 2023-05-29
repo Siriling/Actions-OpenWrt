4c161e59790024dc435ba780eaa17f3c2b767dac
ling <1042585959@qq.com>
2023-05-29 17:57:38 +0800

    更新配置

--- scripts/diy-1.sh
+++ scripts/diy-1.sh
@@ -25,8 +25,8 @@
 # rm -rf openwrt-package/luci-app-verysync
 
 # Add luci-app-unblockneteasemusic
-rm -rf ../../customfeeds/luci/applications/luci-app-unblockmusic
-git clone --depth=1 https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic.git
+# rm -rf ../../customfeeds/luci/applications/luci-app-unblockmusic
+# git clone --depth=1 https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic.git
 
 # Add OpenAppFilter
 # git clone --depth=1 https://github.com/DHDAXCW/OpenAppFilter
